8b994d9a11cef9b435594f4ca9300849309e126a
Semyon Uchvatov <s-uchvatov@yandex.ru>
2021-06-18 10:09:24 +0300

    Changed one dirty hack to another more beautiful one

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { PixieModule } from './pixie.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(PixieModule.register('pixie'));
+  const app = await NestFactory.create(
+    PixieModule.register({ prefix: 'pixie' }),
+  );
   await app.listen(3000);
 }
 
